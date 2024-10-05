692b28479eb310d2716441f7a194c56d04ea3afd
AlexBolychevskiy <alexbol88@mail.ru>
2024-10-05 23:47:23 +0300

    Add files via upload
    Отредактированный 05.10.24 файл

--- module_2_3.py
+++ module_2_3.py
@@ -1,9 +1,14 @@
-my_list = [42,69,322,13,0,99,-5,9,8,7-6,5]
-zero = 0
-while my_list != zero:
-    if my_list[0] < zero and my_list[1] < zero and my_list[2] < zero and my_list[3] < zero  and my_list[4] < zero and my_list[5] < zero and my_list[6] < zero or my_list[0:12] == len(my_list):
-        print(my_list[0], my_list[1], my_list[2], my_list[3],my_list[4], my_list[5], my_list[6])
-        break
-    elif my_list[0] > zero and my_list[1] > zero and my_list[2] > zero and my_list[3]> zero or my_list[4] > zero or my_list[5] > zero:
-        print(my_list[0], my_list[1], my_list[2], my_list[3],  my_list[5])
-        break
+my_list = [42,69,322,13,0,99,-5,9,8,7,-6,5]
+i = 0
+while i < len(my_list):
+            if my_list[i] < 0:
+                i += 1
+                break
+            else:
+                my_list[i] > 0 and my_list[i] != 0
+                print(my_list[i])
+                i += 1
+
+
+
+
