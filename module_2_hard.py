c2d538aad30e3c17d6bf4d49fdcc14d82cb310a4
AlexBolychevskiy <alexbol88@mail.ru>
2024-10-13 21:06:35 +0300

    Add files via upload

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,30 +1,35 @@
 n = int(input('Ввведите число:'))
-a = [(j,i)
-   for i in range(1,20)
-   for j in range(1,i) if n % (j+i) == 0]
+a =[]
+for i in range(1,20):
+    for j in range(1,i):
+                 if n % (i+j) == 0:
+                   a.append([j]+[i])
 c = str(a)
 res_c = c.translate({ord(i): None for i in '[,],(,)'})
 result = res_c.replace(' ','')
-while n >= 3 and n<=20:
+while n >= 3 and n<=20 :
    print(f'{n} -', result)
    break
+#n = int(input('Ввведите число:'))
+#a = [(j,i)
+   #for i in range(1,20)
+   #for j in range(1,i) if n % (j + i) == 0]
+#c = str(a)
+#res_c = c.translate({ord(i): None for i in '[,],(,)'})
+#result = res_c.replace(' ','')
+#while n >= 3 and n<=20 :
+   #print(f'{n} -', result)
+   #break
 #result = f'{j}+{i}'
 #print(result)
 # print(f'{j}+{i}')
-#n = int(input('Ввведите число:'))
-#a =[]
-#for i in range(1,20):
-   # for j in range(1,i):
-                # if n % (j+i) == 0:
-                   #d = f'{j}+{i}={j+i}'
-                   #a.append(d)
-#print(a)
+
 #def get_matrix(n):
     #for i in range(1,20):
         #for j in range(1,i):
-           # if n % (i + j) == 0:
-                #row = []
-                #row.append({j,i})
+            #if n % (j + i) == 0:
+             # row = []
+              #row.append([j]+[i])
     #return row
-#result1 = get_matrix(8)
+#result1 = get_matrix(6)
 #print(result1)
