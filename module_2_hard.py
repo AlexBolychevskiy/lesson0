b8e691413cfa0a87d639924c92133fe83b004ed6
AlexBolychevskiy <alexbol88@mail.ru>
2024-10-14 21:54:12 +0300

    Add files via upload

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,24 +1,24 @@
 n = int(input('Ввведите число:'))
 a =[]
-for i in range(1,20):
-    for j in range(1,i):
-                 if n % (i+j) == 0:
-                   a.append([j]+[i])
+for i in range(1,n):
+    for j in range(i,n):
+                 if n % (i+j) == 0 and i!=j:
+                   a.append([i]+[j])
 c = str(a)
 res_c = c.translate({ord(i): None for i in '[,],(,)'})
 result = res_c.replace(' ','')
-while n >= 3 and n<=20 :
+while n >= 3 and n<=20:
    print(f'{n} -', result)
    break
 #n = int(input('Ввведите число:'))
-#a = [(j,i)
-   #for i in range(1,20)
-   #for j in range(1,i) if n % (j + i) == 0]
+#a = [(i,j)
+   #for i in range(1,n)
+   #for j in range(i,n) if n % (i + j) == 0 and i!=j]
 #c = str(a)
 #res_c = c.translate({ord(i): None for i in '[,],(,)'})
 #result = res_c.replace(' ','')
 #while n >= 3 and n<=20 :
-   #print(f'{n} -', result)
+  # print(f'{n} -', result)
    #break
 #result = f'{j}+{i}'
 #print(result)
