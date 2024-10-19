e63b98645ac8a234177a0dfc77ca6d49ac600c9f
AlexBolychevskiy <alexbol88@mail.ru>
2024-10-19 18:59:55 +0300

    Add files via upload

--- module_3_1.py
+++ module_3_1.py
@@ -12,19 +12,21 @@ def string_info(string):
     string_new1 = tuple(string_new)
     count_calls()
     return string_new1
+
 def is_contains(string,list_to_search):
      c = string.upper()
      uppercase_list = [s.upper() for s in list_to_search]
      if c in uppercase_list:
            result = True
-     elif c not in uppercase_list:
+     else:
+     #elif c not in uppercase_list:
         result = False
-     return result
      count_calls()
-string_info('Capybara')
-string_info('Armageddon')
-is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])
-is_contains('cycle', ['recycling', 'cyclic'])
+     return result
+#string_info('Capybara')
+#string_info('Armageddon')
+#is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])
+#is_contains('cycle', ['recycling', 'cyclic'])
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
 print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
