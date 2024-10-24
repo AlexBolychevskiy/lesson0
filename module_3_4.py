184d0085a7ee05327f88e8f3e3c75a5768e852c2
AlexBolychevskiy <alexbol88@mail.ru>
2024-10-24 15:12:21 +0300

    Add files via upload

--- module_3_4.py
+++ module_3_4.py
@@ -3,12 +3,10 @@ def single_root_words(root_word, *other_words):
     a = root_word.lower()
     b = [s.lower() for s in other_words]
     for i in b:
-        if a in b:
-            #or b.count(a):
-        #or other_words.count(root_word):
-            same_words.append(b)
-        if b.count(a):
-            same_words.append(a)
+        if a in i or i in a:
+            same_words.append(i)
+        #if i in a:
+            #same_words.append(i)
     return same_words
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
